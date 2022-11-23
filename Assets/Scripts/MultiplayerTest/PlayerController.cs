4ee42891b38ba4cea91036c15f4b3666e470d646
Lucas Chalmandrier <lucas.chalmandrier43@gmail.com>
2022-11-23 12:02:15 +0100

    Début de l'implémentation des contrôles des joueurs et de la caméra

--- Assets/Scripts/MultiplayerTest/PlayerController.cs
+++ Assets/Scripts/MultiplayerTest/PlayerController.cs
@@ -1,4 +1,4 @@
-using System.Collections;
+/*using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Mirror;
@@ -12,9 +12,18 @@ public class PlayerController : NetworkBehaviour
     private float jumpHeight = 1.0f;
     private float gravityValue = -9.81f;
 
+    public GameObject _cameraMountPoint;
+
     private void Start()
     {
         controller = gameObject.AddComponent<CharacterController>();
+        if (isLocalPlayer)
+        {
+            Transform cameraTransform = Camera.main.gameObject.transform;  //Find main camera which is part of the scene instead of the prefab
+            cameraTransform.parent = _cameraMountPoint.transform;  //Make the camera a child of the mount point
+            cameraTransform.position = _cameraMountPoint.transform.position;  //Set position/rotation same as the mount point
+            cameraTransform.rotation = _cameraMountPoint.transform.rotation;
+        }
     }
 
     void Update()
@@ -44,3 +53,100 @@ public class PlayerController : NetworkBehaviour
     }
 }
 
+*/
+
+using UnityEngine;
+
+namespace Mirror.Examples.NetworkRoom
+{
+    [RequireComponent(typeof(CapsuleCollider))]
+    [RequireComponent(typeof(CharacterController))]
+    [RequireComponent(typeof(NetworkTransform))]
+    [RequireComponent(typeof(Rigidbody))]
+    public class PlayerController : NetworkBehaviour
+    {
+        public CharacterController characterController;
+
+        void OnValidate()
+        {
+            if (characterController == null)
+                characterController = GetComponent<CharacterController>();
+
+            characterController.enabled = false;
+            GetComponent<Rigidbody>().isKinematic = true;
+            GetComponent<NetworkTransform>().syncDirection = SyncDirection.ClientToServer;
+        }
+
+        public override void OnStartLocalPlayer()
+        {
+            characterController.enabled = true;
+        }
+
+        [Header("Movement Settings")]
+        public float moveSpeed = 8f;
+        public float turnSensitivity = 5f;
+        public float maxTurnSpeed = 100f;
+
+        [Header("Diagnostics")]
+        public float horizontal;
+        public float vertical;
+        public float turn;
+        public float jumpSpeed;
+        public bool isGrounded = true;
+        public bool isFalling;
+        public Vector3 velocity;
+
+        void Update()
+        {
+            if (!isLocalPlayer || characterController == null || !characterController.enabled)
+                return;
+
+            horizontal = Input.GetAxis("Horizontal");
+            vertical = Input.GetAxis("Vertical");
+
+            // Q and E cancel each other out, reducing the turn to zero
+            if (Input.GetKey(KeyCode.Q))
+                turn = Mathf.MoveTowards(turn, -maxTurnSpeed, turnSensitivity);
+            if (Input.GetKey(KeyCode.E))
+                turn = Mathf.MoveTowards(turn, maxTurnSpeed, turnSensitivity);
+            if (Input.GetKey(KeyCode.Q) && Input.GetKey(KeyCode.E))
+                turn = Mathf.MoveTowards(turn, 0, turnSensitivity);
+            if (!Input.GetKey(KeyCode.Q) && !Input.GetKey(KeyCode.E))
+                turn = Mathf.MoveTowards(turn, 0, turnSensitivity);
+
+            if (isGrounded)
+                isFalling = false;
+
+            if ((isGrounded || !isFalling) && jumpSpeed < 1f && Input.GetKey(KeyCode.Space))
+            {
+                jumpSpeed = Mathf.Lerp(jumpSpeed, 1f, 0.5f);
+            }
+            else if (!isGrounded)
+            {
+                isFalling = true;
+                jumpSpeed = 0;
+            }
+        }
+
+        void FixedUpdate()
+        {
+            if (!isLocalPlayer || characterController == null || !characterController.enabled)
+                return;
+
+            transform.Rotate(0f, turn * Time.fixedDeltaTime, 0f);
+
+            Vector3 direction = new Vector3(horizontal, jumpSpeed, vertical);
+            direction = Vector3.ClampMagnitude(direction, 1f);
+            direction = transform.TransformDirection(direction);
+            direction *= moveSpeed;
+
+            if (jumpSpeed > 0)
+                characterController.Move(direction * Time.fixedDeltaTime);
+            else
+                characterController.SimpleMove(direction);
+
+            isGrounded = characterController.isGrounded;
+            velocity = characterController.velocity;
+        }
+    }
+}
